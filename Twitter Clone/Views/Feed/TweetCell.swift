6d1a84e55179f666dcdd748bf7af2362e080c09d
Filip Hercig <fi.hercig@gmail.com>
2022-05-02 08:39:47 +0200

    Fix spacing and add assets

---
+++ Twitter Clone/Views/Feed/TweetCell.swift
@@ -0,0 +1,79 @@
+//
+//  TweetCell.swift
+//  Twitter Clone
+//
+//  Created by Filip Hercig on 26.04.2022..
+//
+
+import SwiftUI
+
+struct TweetCell: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Image("batman")
+                    .resizable()
+                    .scaledToFit()
+                    .clipped()
+                    .frame(width: 56, height: 56)
+                    .cornerRadius(28)
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("Elon Musk")
+                            .font(.system(size: 18, weight: .semibold))
+                        Text("@elonmusk •")
+                            .foregroundColor(.gray)
+                        Text("2w")
+                            .foregroundColor(.gray)
+                    }
+                    Text("I prudcude dope products like Tesla, space rockets and hyperloops becasue I have a ton of money.")
+                }
+            }
+            .padding(.bottom)
+            
+            HStack {
+                Button(action: {}, label: {
+                    Image(systemName: "bubble.left")
+                        .font(.system(size: 16))
+                        .frame(width: 32, height: 32)
+                })
+                
+                Spacer()
+                
+                Button(action: {}, label: {
+                    Image(systemName: "arrow.2.squarepath")
+                        .font(.system(size: 16))
+                        .frame(width: 32, height: 32)
+                })
+                
+                Spacer()
+                
+                Button(action: {}, label: {
+                    Image(systemName: "heart")
+                        .font(.system(size: 16))
+                        .frame(width: 32, height: 32)
+                })
+                
+                Spacer()
+                
+                Button(action: {}, label: {
+                    Image(systemName: "bookmark")
+                        .font(.system(size: 16))
+                        .frame(width: 32, height: 32)
+                })
+            }
+            .foregroundColor(.gray)
+            .padding(.horizontal)
+            
+            Divider()
+        }
+    }
+}
+
+struct TweetCell_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetCell()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro Max"))
+    }
+}
